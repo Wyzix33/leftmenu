0966fb8226f08d149a4f5cd2d72d6adc3e1facb7
Fabriel <wyzixg@yahoo.com>
2022-09-19 16:49:17 +0300

    initial

---
+++ leftmenu.js
@@ -0,0 +1,30 @@
+const mainMenu = document.getElementById('mainMenu');
+mainMenu.removeAttribute('style');
+mainMenu.addEventListener('click', menu);
+mainMenu.addEventListener('mouseleave', menu);
+openMenu_();
+
+function openMenu_() {
+ const lochash = window.location.hash.split('/');
+ const item = mainMenu.querySelector(`a[href='${lochash[0]}']`);
+ if (item) openParent_(item);
+ else openParent_(mainMenu.firstElementChild.firstElementChild);
+}
+
+function openParent_(el) {
+ el.className = 'active';
+ const parentElement = el.parentElement.closest('li');
+ if (parentElement) openParent_(parentElement);
+}
+
+function menu(e) {
+ if (e.type === 'click') mainMenu.classList.add('open');
+ else mainMenu.classList.remove('open');
+ const link = e.target;
+ const closest_ul = link.closest('ul');
+ if (!closest_ul) return;
+ const parallel_active_links = closest_ul.getElementsByClassName('active');
+ [...parallel_active_links].forEach((el) => el.classList.toggle('active'));
+ const closest_li = link.closest('li');
+ if (closest_li) closest_li.classList.toggle('active');
+}
